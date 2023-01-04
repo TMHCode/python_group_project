0252f123d9b01bba7cb7cc304833c8e22b2843f7
TMHCode <49714248+TMHCode@users.noreply.github.com>
2023-01-04 12:28:01 +0100

    added menu

---
+++ menu.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as sg
+
+# Set the size of the window
+sg.ChangeLookAndFeel('DarkAmber')  # Add a touch of color
+sg.SetOptions(font=('Helvetica', 20))
+
+# Create the layout
+layout = [[sg.VPush()],
+          [sg.Push(), sg.Text('Main Menu', size=(30, 1), pad=(10, 20), font=('Helvetica', 50, 'bold'), text_color='white', justification='center'), sg.Push()],
+          [sg.Push(), sg.Button('Tic-Tac-Toe', size=(17, 2), border_width=10, font=('Helvetica', 20), pad=(10, 10)), sg.Push()],
+          [sg.Push(), sg.Button('Connect Four', size=(17, 2), border_width=10, font=('Helvetica', 20), pad=(10, 10)), sg.Push()],
+          [sg.Push(), sg.Button('Rock-Paper-Scissors', size=(17, 2), border_width=10, font=('Helvetica', 20), pad=(10, 10)), sg.Push()],
+          [sg.Push(), sg.Button('Statistics', size=(16, 2), border_width=10, font=('Helvetica', 18), button_color=('black', '#DD6602'), pad=(10, 20)), sg.Push()],
+          [sg.VPush()],
+          [sg.Button('Exit',  size=(7, 2), font=('Helvetica', 16), border_width=10, button_color=('lightgrey', 'darkred'), pad=(0, 0))]]
+
+
+# Create the window
+window = sg.Window('Menu', layout, size=(1100, 700), resizable=True)
+
+# Loop to handle events
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+    print(event, values)
+
+window.close()

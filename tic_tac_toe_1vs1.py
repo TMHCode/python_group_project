e98a21017b0ea4b87eb9997dd82f144fce61ca72
VeeW3499 <VeeW3499@users.noreply.github.com>
2023-01-10 11:38:37 +0100

    add draw and better structure to ttt

--- tic_tac_toe_1vs1.py
+++ tic_tac_toe_1vs1.py
@@ -1,4 +1,43 @@
 import PySimpleGUI as sg
+import menu
+
+from layouts import create_tic_tac_toe_layout
+
+
+def check_win(player, board, window):
+    for row in range(3):
+        if board[row][0] == board[row][1] == board[row][2] and board[row][0] != 0:
+            sg.popup(f"Player {'X' if board[row][0] == 1 else 'O'} has won!")
+            new_field(player, board, window)
+
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != 0:
+            sg.popup(f"Player {'X' if board[0][col] == 1 else 'O'} has won!")
+            new_field(player, board, window)
+
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != 0:
+        sg.popup(f"Player {'X' if board[0][0] == 1 else 'O'} has won!")
+        new_field(player, board, window)
+
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != 0:
+        sg.popup(f"Player {'X' if board[0][2] == 1 else 'O'} has won!")
+        new_field(player, board, window)
+
+
+def check_draw(board):
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                return False
+    return True
+
+
+def new_field(player,board,window):
+    player = -player
+    for row in range(3):
+        for col in range(3):
+            board[row][col] = 0
+            window[(row, col)].update(" ", button_color=("#FCCB53", "#FCCB53"))
 
 
 def main():
@@ -16,7 +55,7 @@ def main():
     # Create a list to store the state of the game
     board = [[0 for _ in range(3)] for _ in range(3)]
 
-    # Create a variable to store the current player (1 = O, -1 = X)
+    # Create a player
     player = 1
 
     # Main game loop
@@ -38,45 +77,16 @@ def main():
 
         # If the event is the "New Game" button, reset the board and switch players
         if event == "New Game":
-            player = -player
-            for row in range(3):
-                for col in range(3):
-                    board[row][col] = 0
-                    window[(row, col)].update(" ", button_color=("#FCCB53", "#FCCB53"))
+            new_field(player, board, window)
 
-        # Check if someone has won the game
-        for row in range(3):
-            if board[row][0] == board[row][1] == board[row][2] and board[row][0] != 0:
-                sg.popup(f"Player {'X' if board[row][0] == 1 else 'O'} has won!")
-                player = -player
-                for row in range(3):
-                    for col in range(3):
-                        board[row][col] = 0
-                        window[(row, col)].update(" ", button_color=("#FCCB53", "#FCCB53"))
-        for col in range(3):
-            if board[0][col] == board[1][col] == board[2][col] and board[0][col] != 0:
-                sg.popup(f"Player {'X' if board[0][col] == 1 else 'O'} has won!")
-                player = -player
-                for row in range(3):
-                    for col in range(3):
-                        board[row][col] = 0
-                        window[(row, col)].update(" ", button_color=("#FCCB53", "#FCCB53"))
-        if board[0][0] == board[1][1] == board[2][2] and board[0][0] != 0:
-            sg.popup(f"Player {'X' if board[0][0] == 1 else 'O'} has won!")
-            player = -player
-            for row in range(3):
-                for col in range(3):
-                    board[row][col] = 0
-                    window[(row, col)].update(" ", button_color=("#FCCB53", "#FCCB53"))
-        if board[0][2] == board[1][1] == board[2][0] and board[0][2] != 0:
-            sg.popup(f"Player {'X' if board[0][2] == 1 else 'O'} has won!")
-            player = -player
-            for row in range(3):
-                for col in range(3):
-                    board[row][col] = 0
-                    window[(row, col)].update(" ", button_color=("#FCCB53", "#FCCB53"))
-    
-    # Close the window
+
+        #Check if someone has won the game
+        check_win(player, board, window)
+        if check_draw(board):
+            sg.popup(f"Draw")
+            new_field(player, board, window)
+
+        # Close the window
     window.close()
 
 

03e1ec1aac9fc3fc34a98c26d03ccf3b429be446
TMHCode <49714248+TMHCode@users.noreply.github.com>
2023-01-14 11:06:38 +0100

    scoreboard visual adjusted

--- scripts/menus/scoreboard_menu.py
+++ scripts/menus/scoreboard_menu.py
@@ -5,7 +5,6 @@ from scripts.menus import main_menu
 from scripts.statistics.stats import load_scores
 
 
-
 def main():
     # get player records from file
     player_records = load_scores()
@@ -21,14 +20,17 @@ def main():
         if event in (None, 'Exit'):
             break
         if event == 'C4':
+            window['high_score_text'].update('High scores! - Connect 4')
             window['TTT_key'].update(visible=False)
             window['RPS_key'].update(visible=False)
             window['C4_key'].update(visible=True)
         elif event == 'RPS':
+            window['high_score_text'].update('High scores! - Rock-Paper-Scissors')
             window['TTT_key'].update(visible=False)
             window['C4_key'].update(visible=False)
             window['RPS_key'].update(visible=True)
         elif event == 'TTT':
+            window['high_score_text'].update('High scores! - Tic-Tac-Toe')
             window['C4_key'].update(visible=False)
             window['RPS_key'].update(visible=False)
             window['TTT_key'].update(visible=True)

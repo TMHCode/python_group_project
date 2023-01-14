7cd69b0cb9f1c683e71dbaa3f4dc576d414a2852
TMHCode <49714248+TMHCode@users.noreply.github.com>
2023-01-14 20:52:28 +0100

    commenting and tidying code #6 (finished)

--- scripts/menus/main_menu.py
+++ scripts/menus/main_menu.py
@@ -6,8 +6,7 @@ from scripts.menus import mode_menu, scoreboard_menu
 This is the main menu file.
 """
 
-# Set the font size
-sg.SetOptions(font=('Helvetica', 15))
+sg.SetOptions(font=('Helvetica', 15))   # set the font size
 
 
 def main():
@@ -18,20 +17,18 @@ def main():
 
     :return:  NO return parameter.
     """
-    # Create the layout
-    layout = create_main_menu_layout()
+    layout = create_main_menu_layout()                                          # create the layout
 
-    # Create the window
-    window = sg.Window('Main Menu', layout, size=(1200, 800), resizable=True)
+    window = sg.Window('Main Menu', layout, size=(1200, 800), resizable=True)   # create the window
 
-    # Loop to handle events
+    # Main Loop
     while True:
-        event, values = window.read()
-        if event in (sg.WIN_CLOSED, 'Quit'):
+        event, values = window.read()                           # get the button click event
+        if event in (sg.WIN_CLOSED, 'Quit'):                    # when Quit button is pressed, go back to the main menu
             break
-        elif event == 'Connect Four':
+        elif event == 'Connect Four':                           # for each of the buttons,
             window.close()
-            mode_menu.main(event)
+            mode_menu.main(event)                               # open the mode menu and give it the name of the game
             break
         elif event == 'Tic-Tac-Toe':
             window.close()
@@ -40,9 +37,9 @@ def main():
         elif event == 'Rock-Paper-Scissors':
             window.close()
             mode_menu.main(event)
-        elif event == 'Statistics':
+        elif event == 'Statistics':                             # if the statistic button is pressed,
             window.close()
-            scoreboard_menu.main()
+            scoreboard_menu.main()                              # open the scoreboard menu
 
-    window.close()
+    window.close()                                              # close the window
 
--- scripts/menus/mode_menu.py
+++ scripts/menus/mode_menu.py
@@ -17,27 +17,24 @@ def main(game):
     :param game: (str)  name of the game. Can be 'ttt', 'c4' or 'rps'
     :return:  NO return parameter.
     """
-    # Create the layout
-    layout = create_mode_menu_layout(game)
+    layout = create_mode_menu_layout(game)                                          # create the layout
 
-    # Create the window
-    window = sg.Window('Game Mode Menu', layout, size=(1200, 800), resizable=True)
+    window = sg.Window('Game Mode Menu', layout, size=(1200, 800), resizable=True)  # create the window
 
-    # Loop to handle events
+    # Main Loop
     while True:
-        event, values = window.read()
-        if event in (sg.WIN_CLOSED, 'Back'):
-
+        event, values = window.read()                           # get the button click event
+        if event in (sg.WIN_CLOSED, 'Back'):                    # when Back button is pressed, go back to the main menu
             window.close()
             main_menu.main()
             break
-        elif event == 'Player vs. Player':
+        elif event == 'Player vs. Player':                      # whatever button is pressed,
             window.close()
-            pvp_name_menu.main(game)
+            pvp_name_menu.main(game)                            # open the respective menu
             break
         elif event == 'Player vs. Bot':
             window.close()
             pvb_name_menu.main(game)
             break
 
-    window.close()
+    window.close()                                              # close the window
